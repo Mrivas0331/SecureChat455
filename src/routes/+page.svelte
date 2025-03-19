31cc9390457038f2a7e26e77b5538155756f5145
Mrivas0331 <Rivas.max11@csu.fullerton.edu>
2025-03-19 08:47:59 -0700

    user can now instantly chat if already logged into website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
+<script lang="js">
+  import { page } from '$app/state';
+</script>
+
 <title>SecureChat</title>
 
 <h1>Welcome to Secure Chat</h1>
 
+{#if page.data.cookies.session}
+  <h2>Already logged in</h2>
+  <a href="/chat"><button>Go to chat</button></a>
+  <a href="/login"><button>Logout</button></a>
+{/if}
+
 <h2>Are you returning?</h2>
 <a href="/login"><button>Login</button></a>
-
-<h2>Or are you newgen?</h2>
-<a href="/signup"><button>Signup</button></a>
