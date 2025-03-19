f73005b16c6ad5c0d18b424aadb0e098873a76fe
Max Rivas <Rivas.max11@csu.fullerton.edu>
2025-03-19 06:30:07 -0700

    finally got the backend for signing up to work, i think

---
+++ src/routes/signup/+page.server.js
@@ -0,0 +1,56 @@
+import { redirect } from "@sveltejs/kit";
+import { io } from "socket.io-client";
+import { hash } from "bcrypt";
+
+export const load = async (event) => {
+  const { cookies } = event;
+  if (cookies.get("flash")) {
+    cookies.delete("flash", { path: "/" });
+  }
+};
+
+export const actions = {
+  signup: async (event) => {
+    const { request, cookies } = event;
+
+    // Verify form data
+    const formData = await request.formData();
+    const username = formData.get("username");
+    const password = formData.get("password");
+    const ws_url = formData.get("ws_url");
+    if (!username || !password || !ws_url) {
+      cookies.set("flash", "signup | error missing", { path: "/" });
+      return JSON.stringify({});
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    // Make HTTPS Request to express server
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // IMPORTANT | This is insecure and is only here for testing with our self-signed certs. Remove when signed with a CA.
+    let https_url = ws_url.replace("wss://", "https://");
+    if (https_url.endsWith("/")) {
+      https_url = https_url.slice(0, -1);
+    }
+    const response = await fetch(`${https_url}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password: hashedPassword }),
+    });
+
+    // Interpret response
+    if (response.status === 201) {
+      const data = await response.json();
+      cookies.set("session", data.session_token, { path: "/", maxAge: 172800 });
+      cookies.set("flash", "signup | successful", { path: "/" });
+      return JSON.stringify({});
+    } else if (response.status === 400) {
+      cookies.set("flash", "signup | error exists", { path: "/" });
+      return JSON.stringify({});
+    } else {
+      cookies.set("flash", "signup | error server", { path: "/" });
+      return JSON.stringify({});
+    }
+  },
+};
