086c452360e0cbdb0f390077ef672f2377359d56
Max Rivas <Rivas.max11@csu.fullerton.edu>
2025-03-20 01:00:44 -0700

    started working w frontend for chats, but i'll finish the rest in the morning

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,31 +1,36 @@
 <script lang="js">
-  import { onMount } from 'svelte';
-  import { page } from '$app/state';
+  import { onMount } from "svelte";
+  import { page } from "$app/state";
+  import { io } from "socket.io-client";
 
-  const { username } = page.data;
+  const username = page.data.username;
+  const session = page.data.session;
+  const socket_url = page.data.socket_url;
 
-  onMount(() => {
+  let msgInputField = "";
+
+  function verifyFlash() {
     // Check for flash cookie
     const flash = page.data.cookies.flash;
     if (flash === undefined) return;
     console.log(`Flash Cookie Found: ${flash}`);
 
     // Check if flash cookie is valid for this page
-    if (flash.split('|').length !== 2) return;
-    if (flash.split('|')[0] !== 'chat ') return;
+    if (flash.split("|").length !== 2) return;
+    if (flash.split("|")[0] !== "chat ") return;
 
     // Interpret flash cookie
-    const flashmsg = flash.split('|')[1];
-    if (flashmsg === ' error invalid') {
-      alert('Error: Missing or invalid session token. Please log in again');
-      window.location.href = '/login';
-    } else if (flashmsg === ' error expired') {
-      alert('Error: Session token expired. Please log in again');
-      window.location.href = '/login';
-    } else if (flashmsg === ' error server') {
-      alert('Internal Server Error');
+    const flashmsg = flash.split("|")[1];
+    if (flashmsg === " error invalid") {
+      alert("Error: Missing or invalid session token. Please log in again");
+      window.location.href = "/login";
+    } else if (flashmsg === " error expired") {
+      alert("Error: Session token expired. Please log in again");
+      window.location.href = "/login";
+    } else if (flashmsg === " error server") {
+      alert("Internal Server Error");
     }
-  });
+  };
 </script>
 
 <title>SecureChat | Chat</title>
