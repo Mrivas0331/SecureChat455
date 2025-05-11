29f5cf5039dbba9eafdee5c11eaaace60eb2b346
Max Rivas <Rivas.max11@csu.fullerton.edu>
2025-05-11 12:18:54 -0700

    last change hopefully

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -147,6 +147,10 @@
   // Run when a user sends a message, adds the message to the appropriate Chat object
   async function recvMessage(sender, reciever, message, encrypted) {
     console.log("received message: ", message);
+    console.log("Encrypted: ", encrypted);
+    console.log("Cipher: ", message.ciphertext);
+    console.log("IV: ", message.iv);
+    alert("Check if works");
     const chat = chats.find(
       (chat) =>
         (chat.user1 === sender && chat.user2 === reciever) ||
